fe05d109cc9cc121dc6cdb94cf7a302af060ac46
robsta7 <meremeev2001@gmail.com>
2020-10-03 21:57:52 +0300

    Add tests task 2

--- main.cpp
+++ main.cpp
@@ -4,5 +4,10 @@
 int main(int argc, const char * argv[])
 {
     assert(task1(10) == 23);
+
+    assert(task2(50) == 44);
+    assert(task2(700) == 798);
+    assert(task2(3000) == 3382);
+
     return 0;
 }
