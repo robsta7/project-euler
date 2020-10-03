a3a776aaa1ecd704187ba57fa915f30680a9319f
robsta7 <meremeev2001@gmail.com>
2020-10-03 21:51:17 +0300

    Add task 2 solution

--- solutions.cpp
+++ solutions.cpp
@@ -13,3 +13,36 @@ size_t arithmetic_progression_sum(size_t a, size_t d, size_t n)
 {
     return n > 0 ? (2 * a + d * (n - 1)) * n / 2 : 0;
 }
+
+size_t task2(size_t value)
+{
+    size_t answer { 0 };
+
+    // Fact: every third Fibonacci number is even.
+
+    // F_{n+3} =
+    // = 2 * F_{n+1} + F_{n} = 3 * F_{n} + 2 * F_{n-1} =
+    // = 3 * F_{n} + F_{n-1} + F_{n-1} =
+    // = 3 * F_{n} + (F_{n-1} + F_{n-2}) + F_{n-3} =
+    // = ... = 4 * F_{n} + F_{n-3}
+
+    // F_{n+3} = 4 * F_{n} + F_{n-3}
+
+    size_t a0 { 0 };
+    size_t a1 { 1 };
+    size_t a2 { 1 };
+    size_t a3 { 2 };
+
+    size_t currentEven { 2 };
+
+    while (currentEven < value)
+    {
+        answer += currentEven;
+        currentEven += 4 * a3 + a0;
+        a3 += a2 + a1;
+        a0 = a1;
+        a1 = a2;
+    }
+
+    return answer;
+}
