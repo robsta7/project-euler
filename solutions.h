854ec3c737f7ce08d02c91874f0483a80da0a0f0
robsta7 <meremeev2001@gmail.com>
2020-10-03 21:08:56 +0300

    Update solutions.h

--- solutions.h
+++ solutions.h
@@ -1,10 +1,12 @@
 #ifndef PROJECT_EULER_SOLUTIONS_H
 #define PROJECT_EULER_SOLUTIONS_H
 
+#include <cstddef>
+
 /// Returns the sum of all numbers less than 'value' multiples of 3 or 5.
-int task1(int value);
+size_t task1(size_t value);
 
 /// Returns the sum of an arithmetic progression.
-int arithmetic_progression_sum(int a, int d, int n);
+size_t arithmetic_progression_sum(size_t a, size_t d, size_t n);
 
 #endif /* PROJECT_EULER_SOLUTIONS_H */
