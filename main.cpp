b44c29833fdc2281e65ddd790dba49ae02a9e267
robsta7 <meremeev2001@gmail.com>
2020-10-04 03:06:46 +0300

    Add tests for multiples_of_3_and_5 function

--- main.cpp
+++ main.cpp
@@ -4,6 +4,8 @@
 int main(int argc, const char * argv[])
 {
     assert(multiples_of_3_and_5(10) == 23);
+    assert(multiples_of_3_and_5(20) == 78);
+    assert(multiples_of_3_and_5(30) == 195);
 
     assert(even_fibonacci_numbers(50) == 44);
     assert(even_fibonacci_numbers(700) == 798);
