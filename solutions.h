2065c9b62572cf6cd8650b1433a4fcaa5fcb900d
robsta7 <meremeev2001@gmail.com>
2020-10-05 12:02:40 +0300

    Update comments solutions.h

--- solutions.h
+++ solutions.h
@@ -12,7 +12,7 @@ size_t arithmetic_progression_sum(size_t a, size_t d, size_t n);
 /// Returns the sum of all even elements of the Fibonacci series that do not exceed 'value'.
 size_t even_fibonacci_numbers(size_t value);
 
-/// Returns largest prime divisor of 'value'.
+/// Returns the largest prime divisor of 'value'.
 long long largest_prime_factor(long long value);
 
 #endif /* PROJECT_EULER_SOLUTIONS_H */
