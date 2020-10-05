faf26f7763fc9b1ab67f58490dc934531cedbaec
robsta7 <meremeev2001@gmail.com>
2020-10-05 15:26:09 +0300

    Update solutions.h

--- solutions.h
+++ solutions.h
@@ -3,16 +3,12 @@
 
 #include <cstddef>
 
-/// Returns the sum of all the multiples of 3 or 5 below 'value'.
 size_t multiples_of_3_and_5(size_t value);
 
-/// Returns the sum of an arithmetic progression.
 size_t arithmetic_progression_sum(size_t a, size_t d, size_t n);
 
-/// Returns the sum of all even elements of the Fibonacci series that do not exceed 'value'.
 size_t even_fibonacci_numbers(size_t value);
 
-/// Returns the largest prime divisor of 'value'.
 long long largest_prime_factor(long long value);
 
-#endif /* PROJECT_EULER_SOLUTIONS_H */
+#endif
