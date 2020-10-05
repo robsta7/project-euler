e70a55d4e2a2fb4365c9c1fffffa0d54aac93fea
robsta7 <meremeev2001@gmail.com>
2020-10-05 15:27:07 +0300

    Update solutions.h

--- solutions.h
+++ solutions.h
@@ -3,12 +3,12 @@
 
 #include <cstddef>
 
-size_t multiples_of_3_and_5(size_t value);
+size_t multiples_of_3_and_5(size_t);
 
-size_t arithmetic_progression_sum(size_t a, size_t d, size_t n);
+size_t arithmetic_progression_sum(size_t, size_t, size_t);
 
-size_t even_fibonacci_numbers(size_t value);
+size_t even_fibonacci_numbers(size_t);
 
-long long largest_prime_factor(long long value);
+long long largest_prime_factor(long long);
 
 #endif
