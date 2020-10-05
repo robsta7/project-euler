11fdd46437a91cd57b76070acee099e8e62ccb96
robsta7 <meremeev2001@gmail.com>
2020-10-05 15:34:12 +0300

    Rename arithmetic_progression_sum function

--- solutions.h
+++ solutions.h
@@ -5,7 +5,7 @@
 
 size_t multiples_of_3_and_5(size_t);
 
-size_t arithmetic_progression_sum(size_t, size_t, size_t);
+size_t ap_sum(size_t, size_t, size_t);
 
 size_t even_fibonacci_numbers(size_t);
 
