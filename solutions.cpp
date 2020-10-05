9ce21ed63b185584131230c0b96ec31ee238b4fe
robsta7 <meremeev2001@gmail.com>
2020-10-05 15:35:07 +0300

    Update solutions.cpp

--- solutions.cpp
+++ solutions.cpp
@@ -3,13 +3,10 @@
 size_t multiples_of_3_and_5(size_t value)
 {
     --value;
-    size_t a { arithmetic_progression_sum(3, 3, value / 3) };
-    size_t b { arithmetic_progression_sum(5, 5, value / 5) };
-    size_t c { arithmetic_progression_sum(15, 15, value / 15) };
-    return  a + b - c;
+    return ap_sum(3, 3, value / 3) + ap_sum(5, 5, value / 5) - ap_sum(15, 15, value / 15);
 }
 
-size_t arithmetic_progression_sum(size_t a, size_t d, size_t n)
+size_t ap_sum(size_t a, size_t d, size_t n)
 {
     return n != 0 ? (2 * a + d * (n - 1)) * n / 2 : 0;
 }
@@ -17,21 +14,8 @@ size_t arithmetic_progression_sum(size_t a, size_t d, size_t n)
 size_t even_fibonacci_numbers(size_t value)
 {
     size_t answer { 0 };
-
-    // Fact: every third Fibonacci number is even.
-
-    // F_{n+3} =
-    // = 2 * F_{n+1} + F_{n} =
-    // = 3 * F_{n} + 2 * F_{n-1} =
-    // = 3 * F_{n} + F_{n-1} + F_{n-1} =
-    // = 3 * F_{n} + (F_{n-1} + F_{n-2}) + F_{n-3} =
-    // = 4 * F_{n} + F_{n-3}
-
-    // F_{n+3} = 4 * F_{n} + F_{n-3}
-
     size_t firstEven { 0 };
     size_t secondEven { 2 };
-
     while (secondEven < value)
     {
         answer += secondEven;
@@ -39,7 +23,6 @@ size_t even_fibonacci_numbers(size_t value)
         secondEven = 4 * secondEven + firstEven;
         firstEven = newFirstEven;
     }
-
     return answer;
 }
 
